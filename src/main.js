eff5e2e3883da87aba2e668fa86e14c0f43851f2
Anna Iwanski <ariwanski@wisc.edu>
2019-05-11 20:15:24 -0500

    linked to html

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   let win = new BrowserWindow({ width: 1920, height: 1080 })
 
-  win.loadFile('index.html')
+  win.loadFile('src/index.html')
 };
 
 app.on('ready', createWindow)
