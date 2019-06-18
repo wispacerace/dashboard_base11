94c1f22503a40949653b0901dd1063c3e04ba485
Unknown <jialuogao980513@gmail.com>
2019-06-17 20:52:17 -0500

    set up some short cut and minor refactoring

--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, globalShortcut } = require('electron');
 
 function createWindow () {
-  let win = new BrowserWindow({ width: 1920, height: 1080 })
-
-  win.loadFile('src/index.html')
+  let win = new BrowserWindow({ width: 1920, height: 1080 });
+  win.loadFile('src/index.html');
+  win.on('closed', function(){
+  	app.quit();
+  });
 };
 
-app.on('ready', createWindow)
+function setup(){
+	globalShortcut.register('F6', () => {
+ 		BrowserWindow.getFocusedWindow().toggleDevTools();
+	});
+	globalShortcut.register('F5', () => {
+ 		BrowserWindow.getFocusedWindow().reload();
+	});
+}
+
+function runApp(){
+	setup();
+	createWindow();
+}
+
+app.on('ready', runApp);
