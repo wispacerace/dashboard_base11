91ace32bb901682bd17434aca8f032db5eb7082c
Anna Iwanski <ariwanski@wisc.edu>
2019-07-11 18:53:08 -0500

    imtegrate main.js code into background.js

--- src/main.js
+++ src/main.js
@@ -8,73 +8,8 @@ Vue.config.productionTip = false
 var packageMap = new Map();
 var packageCount = 0;
 
-function createWindow () {
-  let win = new BrowserWindow({ width: 1920, height: 1080 });
-  win.loadFile('src/index.html');
-  win.on('closed', function(){
-  	app.quit();
-  });
-};
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
-
-function setup(){
-	globalShortcut.register('F6', () => {
- 		BrowserWindow.getFocusedWindow().toggleDevTools();
-	});
-	globalShortcut.register('F5', () => {
- 		BrowserWindow.getFocusedWindow().reload();
-	});
-}
-
-function checkSum(data, checksum){
-	return true;
-}
-/* data format:
-	$WISR,ID,COUNT,UTC_TIME,LATITUDE,LONGITUDE,GPS_ALTITUDE,BAROMETER_ALTITUDE,
-	NUM_SATELLITES,TEMP,BATTERY,EXTRA1,EXTRA2,EXTRA3,EXTRA4,EXTRA5*CHECKSUM
-*/
-function parseData(dataPackage){
-	var dataArray = dataPackage.split(",");
-	var length = dataArray.length;
-	var lastEntry = dataArray[length - 1];
-	lastEntry = lastEntry.split("*");
-
-	var packageID = dataArray[1];
-	var count = dataArray[2];
-	var checksum = lastEntry[1];
-	dataArray[length - 1] = lastEntry[0];
-	if(checkSum(dataArray, checksum) === false){
-		return -1;
-	}
-	
-	dataArray = dataArray.slice(3, );
-	return [count, dataArray];
-}
-
-function running(pause){
-	// setInterval(function () { 
- //    	console.log('Refreshing');
- //    	var dataPackage = ???;
- //    	var result = parseData(dataPackage);
-	 // }, pause);
-	var testdata = "$WISR,ID,COUNT,UTC_TIME,LATITUDE,LONGITUDE,GPS_ALTITUDE,BAROMETER_ALTITUDE,\
-NUM_SATELLITES,TEMP,BATTERY,EXTRA1,EXTRA2,EXTRA3,EXTRA4,EXTRA5*CHECKSUM";
-	var result = parseData(testdata);
-	if(result !== -1){
-		var count = result[0];
-		var dataArray = result[1];
-		console.log(count + "   " + dataArray);
-	}
-}
-function runApp(){
-	setup();
-	createWindow();
-	running(1000); // wait time in ms
-	app.quit();
-}
-
-app.on('ready', runApp);
